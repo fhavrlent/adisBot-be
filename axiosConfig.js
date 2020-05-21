774d784281a321fa5e01214214cccf59565d1a54
Filip Havrlent <filip@havrlent.com>
2020-05-22 00:32:04 +0200

    Fix build (#11)

--- axiosConfig.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-require('dotenv').config();
-
-const instance = axios.create({});
-
-instance.defaults.headers.common['Client-ID'] = process.env.CLIENT_ID;
-
-exports.axios = instance;
